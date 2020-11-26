bf4910566a051112c1df6ac106c7ffe9fa97339d
Andrew Sun <andrew@crossbowffs.com>
2020-11-26 11:25:36 -0800

    Refactor accent color computation

--- Keyboard/UIColor+Accent.swift
+++ Keyboard/UIColor+Accent.swift
@@ -2,22 +2,35 @@ import UIKit
 
 extension UIColor {
     /**
+     * Returns whether this color is considered "dark". May return nil if the color is
+     * in an incompatible color space.
+     */
+    private func isDark() -> Bool? {
+        var white: CGFloat = 0
+        if !self.getWhite(&white, alpha: nil) {
+            return nil
+        }
+        return white < 0.5
+    }
+
+    /**
+     * Returns the user interface style corresponding to this background color.
+     */
+    private func userInterfaceStyle() -> UIUserInterfaceStyle {
+        guard let dark = self.isDark() else { return .unspecified }
+        return dark ? .dark : .light
+    }
+
+    /**
      * Returns an appropriately themed foreground color for this UI element color when
      * applied on top of the given background color.
      */
-    private func fromBackgroundColor(_ color: UIColor) -> UIColor {
+    private func aboveBackground(_ color: UIColor) -> UIColor {
         // We're using the "provider" closure as basically a hacky way to be notified
         // that the system theme may have changed. We ignore the input trait collection
-        // and instead initialize our own based on the immediate background color.
+        // and instead initialize our own based on the actual background color.
         return UIColor(dynamicProvider: { (_: UITraitCollection) -> UIColor in
-            var white: CGFloat = 0
-            color.getWhite(&white, alpha: nil)
-            let traits: UITraitCollection
-            if white < 0.5 {
-                traits = UITraitCollection(userInterfaceStyle: .dark)
-            } else {
-                traits = UITraitCollection(userInterfaceStyle: .light)
-            }
+            let traits = UITraitCollection(userInterfaceStyle: color.userInterfaceStyle())
             return self.resolvedColor(with: traits)
         })
     }
@@ -31,5 +44,5 @@ extension UIColor {
      * The color for text labels that contain primary content when displayed on top
      * of the app-wide accent color.
      */
-    static let accentedLabel = UIColor.label.fromBackgroundColor(accent)
+    static let accentedLabel = UIColor.label.aboveBackground(accent)
 }
